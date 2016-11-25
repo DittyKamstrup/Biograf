fcdd7c31f709c177452cb9d353446ddea35a27b4
Ditte Aarup Kamstrup <ditt4030@edu.easj.dk>
2016-11-25 01:31:47 +0100

    Add project files.

---
+++ Biograf/Model/FilmList.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using Newtonsoft.Json;
+
+namespace Biograf.Model
+{
+    public class FilmList : ObservableCollection<FilmNavn>
+    {
+        public FilmList() : base()
+        {
+            // Tilføj film til programmet
+            this.Add(new FilmNavn() { filmNavn = "Moulin Rouge", pris = 29.95, releaseDato = new DateTime(2001,05, 09), description = "All time best love story", sal = "Sal 5", rating = 10.0 });
+            this.Add(new FilmNavn() { filmNavn = "Mr. Nobody", pris = 39.95, releaseDato = new DateTime(2010, 12, 25), description = "So much rabdom, it's awesome", sal = "Sal 7", rating = 8.5 });
+        }
+
+
+        // Giver mig Jsonformat for FilmList object
+
+        public string GetJson()
+        {
+            string json = JsonConvert.SerializeObject(this);
+            return json;
+        }
+
+
+        // metode som modtager en string af json og desorialiserer til objekter af film
+
+        /// <param name="jsonText"></param>
+        public void IndsætJson(string jsonText)
+        {
+            List<FilmNavn> nyListe = JsonConvert.DeserializeObject<List<FilmNavn>>(jsonText);
+
+            foreach (var film in nyListe)
+            {
+                this.Add(film);
+            }
+
+
+        }
+    }
+}
---
+++ Biograf/ViewModel/FilmViewModel.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using Biograf.Model;
+using Newtonsoft.Json;
+using Windows.Storage;
+using Windows.UI.Popups;
+using System.IO;
+
+namespace Biograf.ViewModel
+{
+    public class FilmViewModel : INotifyPropertyChanged
+    {
+        public Model.FilmList Filmliste { get; set; }
+        private Model.FilmNavn selectedFilm;
+
+        //Jeg laver objekter af RelayCommand for at genbruge kode
+        public RelayCommand AddFilmRelayCommand { get; set; }
+        public RelayCommand RemoveFilmRelayCommand { get; set; }
+        public RelayCommand SaveFilmCommand { get; private set; }
+        public RelayCommand DeleteAllFilmCommand { get; private set; }
+        public RelayCommand HentDataCommand { get; private set; }
+
+        StorageFolder localfolder = null;
+
+        private readonly string filnavn = "JsonText.jsonNY1";
+
+        // Eksempel til brug af klasser til commands
+        //public AddFilmCommand AddFilmCommand { get; set; }
+        // public RemoveFilmCommand RemoveFilmCommand { get; set; }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Model.FilmNavn SelectedFilm
+        {
+            get { return selectedFilm; }
+            set
+            {
+                selectedFilm = value;
+                OnPropertyChanged(nameof(SelectedFilm));
+            }
+        }
+
+        public Model.FilmNavn NewFilm { get; set; }
+
+
+        public FilmViewModel()
+        {
+            Filmliste = new Model.FilmList();
+            selectedFilm = new Model.FilmNavn();
+            AddFilmRelayCommand = new RelayCommand(AddNewFilm);
+            RemoveFilmRelayCommand = new RelayCommand(RemoveNewFilm);
+
+            SaveFilmCommand = new RelayCommand(GemDataTilDiskAsync);
+            HentDataCommand = new RelayCommand(HentdataFraDiskAsync);
+
+            DeleteAllFilmCommand = new RelayCommand(() => this.Filmliste.Clear());
+
+            NewFilm = new Model.FilmNavn();
+
+            // Eksempel til brug af klasser til commands
+            // AddFilmCommand = new AddFilmCommand(AddNewFilm);
+            // RemoveFilmCommand = new RemoveFilmCommand(RemoveNewFilm);
+
+            localfolder = ApplicationData.Current.LocalFolder;
+
+        }
+
+        public async void HentdataFraDiskAsync()
+        {
+            try
+            {
+                StorageFile file = await localfolder.GetFileAsync(filnavn);
+                string jsonText = await FileIO.ReadTextAsync(file);
+                this.Filmliste.Clear();
+                Filmliste.IndsætJson(jsonText);
+            }
+            catch (FileNotFoundException)
+            {
+                MessageDialog messageDialog = new MessageDialog("Har du husket at gemme?", "Fil ikke fundet");
+                await messageDialog.ShowAsync();
+            }
+
+        }
+
+        // Gemmer json data fra liste i localfolder
+        public async void GemDataTilDiskAsync()
+        {
+
+
+            string jsonText = this.Filmliste.GetJson();
+            StorageFile file = await localfolder.CreateFileAsync(filnavn, CreationCollisionOption.ReplaceExisting);
+            await FileIO.WriteTextAsync(file, jsonText);
+        }
+
+
+
+
+        protected virtual void OnPropertyChanged(string PropertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(PropertyName));
+            }
+        }
+
+
+        public void AddNewFilm()
+        {
+            Filmliste.Add(NewFilm);
+        }
+
+        public void RemoveNewFilm()
+        {
+            Filmliste.Remove(SelectedFilm);
+        }
+    }
+}
